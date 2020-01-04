b67af01eafd4b77358c42a46aabdece3883bc87f
koalatea <koalateac@gmail.com>
2020-01-03 18:01:38 -0600

    basic database stuff begone

---
+++ cmd/devtools/createusers/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/koalatea/authserver/ent"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	client, err := ent.Open("sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
+	if err != nil {
+		log.Fatalf("failed opening connection to sqlite: %v", err)
+	}
+	defer client.Close()
+	// run the auto migration tool.
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+
+	_, err = createUser(context.Background(), client)
+	if err != nil {
+		log.Fatalf("failed to create user %v", err)
+	}
+}
+
+func createUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
+	u, err := client.User.
+		Create().
+		SetUsername("a8m").
+		SetPassword("a8m").
+		Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating user: %v", err)
+	}
+	log.Println("user was created: ", u)
+	return u, nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/koalatea/authserver
 
 go 1.13
+
+require (
+	github.com/facebookincubator/ent v0.0.0-20200102164226-46982d2f0515
+	github.com/mattn/go-sqlite3 v1.11.0
+)
