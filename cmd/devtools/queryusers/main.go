71a91dc113c90576729c16115a1a8350b2947341
koalatea <koalateac@gmail.com>
2020-01-07 21:56:25 -0600

    basic schema manipulation in devscripts

---
+++ cmd/devtools/queryusers/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/koalatea/authserver/ent"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	// client, err := ent.Open("sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
+	client, err := ent.Open("sqlite3", "file:test.sql?_fk=1")
+	if err != nil {
+		log.Fatalf("failed opening connection to sqlite: %v", err)
+	}
+	defer client.Close()
+
+	err = queryUsers(context.Background(), client)
+	if err != nil {
+		log.Fatalf("failed to create user %v", err)
+	}
+}
+
+func queryUsers(ctx context.Context, client *ent.Client) error {
+	users, err := client.User.
+		Query().All(ctx) // what happens with only and multiple exist?
+	if err != nil {
+		return fmt.Errorf("failed querying user: %v", err)
+	}
+	for _, u := range users {
+		fmt.Printf("found user %s\n", u.Username)
+	}
+	return nil
+}
