e19e9fd3da55260ac08fc09389646726dd313e0b
Ryan Whittier <Koalateac@gmail.com>
2022-12-06 09:38:20 +0000

    Added test to continue to confirm that id_token works

---
+++ server/oidc/oidc_test.go
@@ -0,0 +1,73 @@
+package oidc
+
+import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strings"
+	"testing"
+
+	"github.com/koalatea/authserver/server/ent"
+	"github.com/koalatea/authserver/server/ent/migrate"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Response struct {
+	AccessToken string `json:"access_token"`
+	ExpiresIn   int    `json:"expires_in"`
+	IDToken     string `json:"id_token"`
+	Scope       string `json:"scope"`
+	TokenType   string `json:"token_type"`
+}
+
+func TestGettingIDToken(t *testing.T) {
+
+	// graph := enttest.Open(t, "sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
+	graph, _ := ent.Open("sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
+	graph.Schema.Create(context.Background(), migrate.WithGlobalUniqueID(true))
+
+	provider := NewOIDCProvider(graph)
+	router := http.NewServeMux()
+	provider.RegisterHandlers(router)
+	w := httptest.NewRecorder()
+
+	// these values are hardcoded for testing
+	v := url.Values{}
+	v.Set("username", "peter")
+	v.Set("scopes", "openid")
+	r, _ := http.NewRequest("POST", "/oidc/auth?client_id=my-client&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fcallback&response_type=code&scope=photos+openid+offline&state=some-random-state-foobar&nonce=some-random-nonce", strings.NewReader(v.Encode()))
+	r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	// Get auth code
+	router.ServeHTTP(w, r)
+	locationString := w.Header().Get("Location")
+	u, err := url.Parse(locationString)
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+	m, _ := url.ParseQuery(u.RawQuery)
+	code := m.Get("code")
+	// fmt.Printf("code: %s\n", code)
+
+	// use auth code for a new token
+	v = url.Values{}
+	v.Set("client_id", "my-client")
+	v.Set("client_secret", "foobar")
+	v.Set("grant_type", "authorization_code")
+	v.Set("code", code)
+	w = httptest.NewRecorder()
+	r, _ = http.NewRequest("POST", "/oidc/token", strings.NewReader(v.Encode()))
+	r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	// r.SetBasicAuth(url.QueryEscape("my-client"), url.QueryEscape("foobar"))
+
+	router.ServeHTTP(w, r)
+	response := &Response{}
+	body, _ := ioutil.ReadAll(w.Body)
+	json.Unmarshal(body, response)
+	if response.IDToken == "" {
+		t.Error("what")
+	}
+}
--- server/oidc/storage.go
+++ server/oidc/storage.go
@@ -2,7 +2,6 @@ package oidc
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"github.com/koalatea/authserver/server/ent"
@@ -29,7 +28,7 @@ type OIDCStorage struct {
 // CreateOpenIDConnectSession creates an open id connect session
 // for a given authorize code. This is relevant for explicit open id connect flow.
 func (o *OIDCStorage) CreateOpenIDConnectSession(ctx context.Context, authorizeCode string, requester fosite.Requester) error {
-	fmt.Println("\n\nDOES CreateOpenIDConnectSession RUN?\n\n")
+	//fmtln("\n\nDOES CreateOpenIDConnectSession RUN?")
 	accessRequest, err := o.client.AccessRequest.
 		Create().
 		SetRequestedScopes(requester.GetRequestedScopes()).
@@ -41,7 +40,7 @@ func (o *OIDCStorage) CreateOpenIDConnectSession(ctx context.Context, authorizeC
 		SetActive(true).
 		Save(ctx)
 	if err != nil {
-		fmt.Printf("%w\n", err)
+		//fmtf("%s\n", err)
 		return err
 	}
 
@@ -57,7 +56,7 @@ func (o *OIDCStorage) CreateOpenIDConnectSession(ctx context.Context, authorizeC
 		SetAuthTime(session.Claims.AuthTime).
 		Save(ctx)
 	if err != nil {
-		fmt.Printf("%w\n", err)
+		//fmtf("%s\n", err)
 		return err
 	}
 
@@ -67,7 +66,7 @@ func (o *OIDCStorage) CreateOpenIDConnectSession(ctx context.Context, authorizeC
 		SetSession(oidcSession).
 		Save(ctx)
 	if err != nil {
-		fmt.Printf("%w\n", err)
+		//fmtf("%s\n", err)
 		return err
 	}
 	return nil
@@ -78,9 +77,10 @@ func (o *OIDCStorage) CreateOpenIDConnectSession(ctx context.Context, authorizeC
 // - ErrNoSessionFound if no session was found
 // - or an arbitrary error if an error occurred.
 func (o *OIDCStorage) GetOpenIDConnectSession(ctx context.Context, authorizeCode string, requester fosite.Requester) (fosite.Requester, error) {
-	fmt.Println("\n\nDOES GetOpenIDConnectSessio RUN?\n\n")
+	//fmtln("\n\nDOES GetOpenIDConnectSessio RUN?")
 	sess, err := o.client.OIDCAuthCode.Query().Where(oidcauthcode.AuthorizationCode(authorizeCode)).QuerySession().Only(ctx)
 	if err != nil {
+		//fmtf("%s\n", err)
 		return nil, err
 	}
 	session := &openid.DefaultSession{
@@ -104,6 +104,7 @@ func (o *OIDCStorage) GetOpenIDConnectSession(ctx context.Context, authorizeCode
 	// req.Merge(requester)
 	req.Client = temp
 	_ = req.GetRequestForm().Get("code")
+	//fmtf("%+v\n%+v\n%+v\n%+v\n", req, req.Client, req.GetSession(), session.Claims)
 	return req, nil
 }
 
@@ -118,7 +119,7 @@ func (o *OIDCStorage) DeleteOpenIDConnectSession(ctx context.Context, authorizeC
 	// if err != nil {
 	// 	return err
 	// }
-	fmt.Println("\n\nDOES THIS RUN2?\n\n")
+	//fmtln("\n\nDOES THIS RUN2?")
 	return nil
 }
 
@@ -137,22 +138,22 @@ func (o *OIDCStorage) GetClient(_ context.Context, id string) (fosite.Client, er
 }
 
 func (o *OIDCStorage) SetTokenLifespans(clientID string, lifespans *fosite.ClientLifespanConfig) error {
-	fmt.Println("\n\nDOES THIS RUN3?\n\n")
+	//fmtln("\n\nDOES THIS RUN3?")
 	return fosite.ErrNotFound
 }
 
 func (o *OIDCStorage) ClientAssertionJWTValid(_ context.Context, jti string) error {
-	fmt.Println("\n\nDOES THIS RUN4?\n\n")
+	//fmtln("\n\nDOES THIS RUN4?")
 	return nil
 }
 
 func (o *OIDCStorage) SetClientAssertionJWT(_ context.Context, jti string, exp time.Time) error {
-	fmt.Println("\n\nDOES THIS RUN5?\n\n")
+	//fmtln("\n\nDOES THIS RUN5?")
 	return nil
 }
 
 func (o *OIDCStorage) CreateAuthorizeCodeSession(ctx context.Context, code string, req fosite.Requester) error {
-	fmt.Println("\n\nDOES THIS RUN6?\n\n")
+	//fmtln("\n\nDOES THIS RUN6?")
 	accessRequest, err := o.client.AccessRequest.
 		Create().
 		SetRequestedScopes(req.GetRequestedScopes()).
@@ -164,7 +165,7 @@ func (o *OIDCStorage) CreateAuthorizeCodeSession(ctx context.Context, code strin
 		SetActive(true).
 		Save(ctx)
 	if err != nil {
-		fmt.Printf("%w\n", err)
+		//fmtf("%s\n", err)
 		return err
 	}
 
@@ -180,7 +181,7 @@ func (o *OIDCStorage) CreateAuthorizeCodeSession(ctx context.Context, code strin
 		SetAuthTime(session.Claims.AuthTime).
 		Save(ctx)
 	if err != nil {
-		fmt.Printf("%w\n", err)
+		//fmtf("%s\n", err)
 		return err
 	}
 
@@ -190,7 +191,7 @@ func (o *OIDCStorage) CreateAuthorizeCodeSession(ctx context.Context, code strin
 		SetSession(oidcSession).
 		Save(ctx)
 	if err != nil {
-		fmt.Printf("%w\n", err)
+		//fmtf("%s\n", err)
 		return err
 	}
 	return nil
@@ -198,11 +199,11 @@ func (o *OIDCStorage) CreateAuthorizeCodeSession(ctx context.Context, code strin
 
 func (o *OIDCStorage) GetAuthorizeCodeSession(ctx context.Context, code string, _ fosite.Session) (fosite.Requester, error) {
 	// url.ParseQuery(r.Form)
-	// fmt.Printf("\n\nAUTHCODE %s\n\n", code)
-	// fmt.Println("\n\nDOES THIS RUN7?\n\n") // yes
+	// //fmtf("\n\nAUTHCODE %s", code)
+	// //fmtln("\n\nDOES THIS RUN7?") // yes
 	sess, err := o.client.OIDCAuthCode.Query().Where(oidcauthcode.AuthorizationCode(code)).QuerySession().Only(ctx)
 	if err != nil {
-		fmt.Printf("%w", err)
+		//fmtf("%s", err)
 		return nil, err
 	}
 	session := &openid.DefaultSession{
@@ -228,17 +229,17 @@ func (o *OIDCStorage) GetAuthorizeCodeSession(ctx context.Context, code string, 
 }
 
 func (o *OIDCStorage) InvalidateAuthorizeCodeSession(ctx context.Context, code string) error {
-	fmt.Println("\n\nDOES THIS RUN8?\n\n")
+	//fmtln("\n\nDOES THIS RUN8?")
 	return nil
 }
 
 func (o *OIDCStorage) CreatePKCERequestSession(_ context.Context, code string, req fosite.Requester) error {
-	fmt.Println("\n\nDOES THIS RUN9?\n\n") // yes but why
+	//fmtln("\n\nDOES THIS RUN9?") // yes but why
 	return nil
 }
 
 func (o *OIDCStorage) GetPKCERequestSession(_ context.Context, code string, _ fosite.Session) (fosite.Requester, error) {
-	fmt.Println("\n\nDOES THIS RUN10?\n\n")
+	//fmtln("\n\nDOES THIS RUN10?")
 	req := fosite.NewAuthorizeRequest()
 	temp, _ := o.GetClient(context.Background(), "my-client")
 	req.Client = temp
@@ -261,100 +262,100 @@ func (o *OIDCStorage) GetPKCERequestSession(_ context.Context, code string, _ fo
 }
 
 func (o *OIDCStorage) DeletePKCERequestSession(_ context.Context, code string) error {
-	fmt.Println("\n\nDOES THIS RUN11?\n\n")
+	//fmtln("\n\nDOES THIS RUN11?")
 	return nil
 }
 
 func (o *OIDCStorage) CreateAccessTokenSession(_ context.Context, signature string, req fosite.Requester) error {
-	fmt.Println("\n\nDOES THIS RUN12?\n\n")
+	//fmtln("\n\nDOES THIS RUN12?")
 	return nil
 }
 
 func (o *OIDCStorage) GetAccessTokenSession(_ context.Context, signature string, _ fosite.Session) (fosite.Requester, error) {
-	fmt.Println("\n\nDOES THIS RUN13?\n\n")
+	//fmtln("\n\nDOES THIS RUN13?")
 	return nil, nil
 }
 
 func (o *OIDCStorage) DeleteAccessTokenSession(_ context.Context, signature string) error {
-	fmt.Println("\n\nDOES THIS RUN14?\n\n")
+	//fmtln("\n\nDOES THIS RUN14?")
 	return nil
 }
 
 func (o *OIDCStorage) CreateRefreshTokenSession(_ context.Context, signature string, req fosite.Requester) error {
-	fmt.Println("\n\nDOES THIS RUN15?\n\n")
+	//fmtln("\n\nDOES THIS RUN15?")
 	return nil
 }
 
 func (o *OIDCStorage) GetRefreshTokenSession(_ context.Context, signature string, _ fosite.Session) (fosite.Requester, error) {
-	fmt.Println("\n\nDOES THIS RUN16?\n\n")
+	//fmtln("\n\nDOES THIS RUN16?")
 	return nil, nil
 }
 
 func (o *OIDCStorage) DeleteRefreshTokenSession(_ context.Context, signature string) error {
-	fmt.Println("\n\nDOES THIS RUN17?\n\n")
+	//fmtln("\n\nDOES THIS RUN17?")
 	return nil
 }
 
 func (o *OIDCStorage) Authenticate(_ context.Context, name string, secret string) error {
-	fmt.Println("\n\nDOES THIS RUN18?\n\n")
+	//fmtln("\n\nDOES THIS RUN18?")
 	return nil
 }
 
 func (o *OIDCStorage) RevokeRefreshToken(ctx context.Context, requestID string) error {
-	fmt.Println("\n\nDOES THIS RUN19?\n\n")
+	//fmtln("\n\nDOES THIS RUN19?")
 	return nil
 }
 
 func (o *OIDCStorage) RevokeRefreshTokenMaybeGracePeriod(ctx context.Context, requestID string, signature string) error {
 	// no configuration option is available; grace period is not available with memory store
-	fmt.Println("\n\nDOES THIS RUN20?\n\n")
+	//fmtln("\n\nDOES THIS RUN20?")
 	return nil
 }
 
 func (o *OIDCStorage) RevokeAccessToken(ctx context.Context, requestID string) error {
-	fmt.Println("\n\nDOES THIS RUN21?\n\n")
+	//fmtln("\n\nDOES THIS RUN21?")
 	return nil
 }
 
 func (o *OIDCStorage) GetPublicKey(ctx context.Context, issuer string, subject string, keyId string) (*jose.JSONWebKey, error) {
-	fmt.Println("\n\nDOES THIS RUN22?\n\n")
+	//fmtln("\n\nDOES THIS RUN22?")
 	return nil, nil
 }
 func (o *OIDCStorage) GetPublicKeys(ctx context.Context, issuer string, subject string) (*jose.JSONWebKeySet, error) {
-	fmt.Println("\n\nDOES THIS RUN23?\n\n")
+	//fmtln("\n\nDOES THIS RUN23?")
 	return nil, nil
 }
 
 func (o *OIDCStorage) GetPublicKeyScopes(ctx context.Context, issuer string, subject string, keyId string) ([]string, error) {
-	fmt.Println("\n\nDOES THIS RUN24?\n\n")
+	//fmtln("\n\nDOES THIS RUN24?")
 	return nil, nil
 }
 
 func (o *OIDCStorage) IsJWTUsed(ctx context.Context, jti string) (bool, error) {
-	fmt.Println("\n\nDOES THIS RUN25?\n\n")
+	//fmtln("\n\nDOES THIS RUN25?")
 	return false, nil
 }
 
 func (o *OIDCStorage) MarkJWTUsedForTime(ctx context.Context, jti string, exp time.Time) error {
-	fmt.Println("\n\nDOES THIS RUN26?\n\n")
+	//fmtln("\n\nDOES THIS RUN26?")
 	return nil
 }
 
 // CreatePARSession stores the pushed authorization request context. The requestURI is used to derive the key.
 func (o *OIDCStorage) CreatePARSession(ctx context.Context, requestURI string, request fosite.AuthorizeRequester) error {
-	fmt.Println("\n\nDOES THIS RUN27?\n\n")
+	//fmtln("\n\nDOES THIS RUN27?")
 	return nil
 }
 
 // GetPARSession gets the push authorization request context. If the request is nil, a new request object
 // is created. Otherwise, the same object is updated.
 func (o *OIDCStorage) GetPARSession(ctx context.Context, requestURI string) (fosite.AuthorizeRequester, error) {
-	fmt.Println("\n\nDOES THIS RUN28?\n\n")
+	//fmtln("\n\nDOES THIS RUN28?")
 	return nil, nil
 }
 
 // DeletePARSession deletes the context.
 func (o *OIDCStorage) DeletePARSession(ctx context.Context, requestURI string) (err error) {
-	fmt.Println("\n\nDOES THIS RUN29?\n\n")
+	//fmtln("\n\nDOES THIS RUN29?")
 	return nil
 }
